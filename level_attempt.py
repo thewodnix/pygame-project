5d1989d9ee0fca348ff4d8e4a68b57dd3cd7477e
timur <timurtimber@gmail.com>
2025-01-20 22:51:10 +0300

    Tried to do the pac-man movement

--- level_attempt.py
+++ level_attempt.py
@@ -154,7 +154,6 @@ class Player(pygame.sprite.Sprite):
             else:
                 self.y -= self.speed
             self.rect.y = self.y
-
         #     self.isJump = True
         #     if self.isJump is True:
         #         if self.jumpCount >= -10:
